45797dd65cdf5803844343fd6d47da59c7851c49
andre.orvalho <andre.orvalho@gmail.com>
2022-12-28 18:05:10 +0200

    now using the bip32 instead of ecpair

--- build/index.js
+++ build/index.js
@@ -1,22 +1,27 @@
 "use strict";
-// const ecc = require('tiny-secp256k1')
-// const { BIP32Factory } = require('bip32')
-// const bip39 = require('bip39')
-// // You must wrap a tiny-secp256k1 compatible implementation
-// const bip32 = BIP32Factory(ecc)
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const ecpair_1 = __importDefault(require("ecpair"));
+//import bitcoin from 'bitcoinjs-lib';
+const bitcoin = require('bitcoinjs-lib');
 const ecc = require('tiny-secp256k1');
-// const mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost'
-// const seed = bip39.mnemonicToSeed(mnemonic)
-// const node = bip32.fromSeed(seed)
-// console.log(node.toBase58())
-const ECPair = (0, ecpair_1.default)(ecc);
-const keyPair = ECPair.makeRandom();
-//const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
-const publicKey = keyPair.publicKey.toString('hex');
-const privateKey = keyPair.toWIF();
-console.log({ privateKey, publicKey });
+const { BIP32Factory } = require('bip32');
+const bip39 = require('bip39');
+const bip32 = BIP32Factory(ecc);
+const mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost';
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+const node = bip32.fromSeed(seed);
+const strng = node.toBase58();
+const restored = bip32.fromBase58(strng);
+function getAddress(node, network) {
+    return bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address;
+}
+console.log(getAddress(node));
+console.log(getAddress(restored));
+//import bitcoin from 'bitcoinjs-lib';
+// import ECPairFactory from 'ecpair';
+// const ecc = require('tiny-secp256k1')
+// const ECPair = ECPairFactory(ecc);
+// const keyPair = ECPair.makeRandom();
+// const publicKey = keyPair.publicKey.toString('hex');
+// const privateKey = keyPair.toWIF();
+// console.log({ privateKey, publicKey });
+//var message = 'Hey this is Ranchi Mall'
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-// const ecc = require('tiny-secp256k1')
-// const { BIP32Factory } = require('bip32')
-// const bip39 = require('bip39')
-// // You must wrap a tiny-secp256k1 compatible implementation
-// const bip32 = BIP32Factory(ecc)
-
-import bitcoin from 'bitcoinjs-lib';
-import ECPairFactory from 'ecpair';
+//import bitcoin from 'bitcoinjs-lib';
+const bitcoin = require('bitcoinjs-lib');
 const ecc = require('tiny-secp256k1')
+const { BIP32Factory } = require('bip32')
+const bip39 = require('bip39')
+const bip32 = BIP32Factory(ecc)
 
-// const mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost'
-// const seed = bip39.mnemonicToSeed(mnemonic)
-// const node = bip32.fromSeed(seed)
-// console.log(node.toBase58())
+const mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost';
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+const node = bip32.fromSeed(seed);
+const strng = node.toBase58();
+const restored = bip32.fromBase58(strng);
 
-const ECPair = ECPairFactory(ecc);
-const keyPair = ECPair.makeRandom();
-//const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
-const publicKey = keyPair.publicKey.toString('hex');
-const privateKey = keyPair.toWIF();
-console.log({ privateKey, publicKey });
+function getAddress(node: any, network?: any): string {
+  return bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address!;
+}
+
+console.log(getAddress(node));
+console.log(getAddress(restored));
+
+//import bitcoin from 'bitcoinjs-lib';
+// import ECPairFactory from 'ecpair';
+// const ecc = require('tiny-secp256k1')
+
+
+// const ECPair = ECPairFactory(ecc);
+// const keyPair = ECPair.makeRandom();
+// const publicKey = keyPair.publicKey.toString('hex');
+// const privateKey = keyPair.toWIF();
+// console.log({ privateKey, publicKey });
+
+
+//var message = 'Hey this is Ranchi Mall'
