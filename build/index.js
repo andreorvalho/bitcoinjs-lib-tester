ea8e4b99066a6df25ea66703a4bc309f6cf25044
andre.orvalho <andre.orvalho@gmail.com>
2022-12-29 16:46:00 +0200

    signature working

--- build/index.js
+++ build/index.js
@@ -1,12 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinMessage = require('bitcoinjs-message');
+const ecc = require('tiny-secp256k1');
 const bip39 = require('bip39');
-const hdkey = require('hdkey');
-const mnemonic = bip39.generateMnemonic(); //generates a 12 word mnemonic
-const seed = bip39.mnemonicToSeed(mnemonic); //creates seed buffer
-const root = hdkey.fromMasterSeed(seed); //should not be passing a promise into here
-const masterPrivateKey = root.privateKey.toString('hex');
-const addrnode = root.derive("m/44'/60'/0'/0/0");
-console.log(addrnode);
-console.log(masterPrivateKey);
-var message = 'Hey this is Ranchi Mall';
+const { BIP32Factory } = require('bip32');
+const bip32 = BIP32Factory(ecc);
+const ecpair_1 = __importDefault(require("ecpair"));
+const ECPair = (0, ecpair_1.default)(ecc);
+const keyPair = ECPair.makeRandom();
+const publicKey = keyPair.publicKey.toString('hex');
+const privateKey = keyPair.toWIF();
+console.log({ privateKey, publicKey });
+function getAddress(publicKey, network) {
+    return bitcoin.payments.p2pkh({ pubkey: publicKey, network }).address;
+}
+var message = 'This is an example of a signed message.';
+var signature = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed);
+console.log(signature.toString('base64'));
+console.log(bitcoinMessage.verify(message, getAddress(keyPair.publicKey), signature));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinMessage = require('bitcoinjs-message')
+const ecc = require('tiny-secp256k1')
+const bip39 = require('bip39')
+const { BIP32Factory } = require('bip32')
+const bip32 = BIP32Factory(ecc)
+import ECPairFactory from 'ecpair';
+
+const ECPair = ECPairFactory(ecc);
+const keyPair = ECPair.makeRandom();
+
+function getAddress(publicKey: any, network?: any): string {
+  return bitcoin.payments.p2pkh({ pubkey: publicKey, network }).address!;
+}
+
+var message = 'This is an example of a signed message.'
+
+var signature = bitcoinMessage.sign(message, keyPair.privateKey, keyPair.compressed)
+console.log(signature.toString('base64'))
+
+console.log(bitcoinMessage.verify(message, getAddress(keyPair.publicKey), signature))
+
 
